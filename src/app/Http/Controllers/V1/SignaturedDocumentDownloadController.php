b5630208fa8943f7689dcf5226843dbdbd4c2704
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-08-31 20:10:25 +0700

    Clean code to reduce complexity

--- src/app/Http/Controllers/V1/SignaturedDocumentDownloadController.php
+++ src/app/Http/Controllers/V1/SignaturedDocumentDownloadController.php
@@ -16,49 +16,66 @@ class SignaturedDocumentDownloadController extends Controller
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  String $id
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request, $id)
     {
-        $document = DocumentSignature::find($id);
-        if (!$document) {
-            $this->kafkaPublish('analytic_event', [
-                'event' => 'download_signed_letter',
-                'status' => KafkaStatusTypeEnum::FAILED()
-            ]);
+        $logData = [
+            'event' => 'download_signed_letter',
+            'status' => KafkaStatusTypeEnum::SUCCESS(),
+        ];
 
+        $document = DocumentSignature::find($id);
+        if ($document) {
+            $file = $this->generateFile($document);
+            $logData['letter']['file'] = $file;
+            $this->kafkaPublish('analytic_event', $logData);
+            return response()->json([
+                'file' => $file
+            ], 200);
+        } else {
+            $logData['status'] = KafkaStatusTypeEnum::FAILED();
+            $this->kafkaPublish('analytic_event', $logData);
             return response()->json([
                 'message' => 'Document not found'
             ], 404);
         }
+    }
 
+    /**
+     * Generate document file.
+     *
+     * @param  DocumentSignature $document
+     * @return String
+     */
+    protected function generateFile($document)
+    {
         $path = config('sikd.base_path_file');
-        // New data with registered flow
-        if ($document->is_registered === true) {
-            $file = $path . 'ttd/sudah_ttd/' . $document->file;
-        } elseif ($document->is_registered === false && $document->is_registered !== null) {
-            $file = $path . 'ttd/draft/' . $document->tmp_draft_file;
-        } else {
-            // Old data without registered flow
-            $file = $document->checkFile($path . 'ttd/draft/' . $document->tmp_draft_file);
-            if ($file === false) {
-                $file = $document->checkFile($path . 'ttd/sudah_ttd/' . $document->file);
-                if ($file === false) { // handle for old data before draft schema implemented
-                    $file = $path . 'ttd/blm_ttd/' . $document->file;
-                }
+        $file = match ($document->is_registered) {
+            (int) true => $path . 'ttd/sudah_ttd/' . $document->file,
+            (int) false => $file = $path . 'ttd/draft/' . $document->tmp_draft_file,
+            default => $this->generateFileWhichRegisteredStatusNull($document)
+        };
+        return $file;
+    }
+
+    /**
+     * Generate document file which has null registered status.
+     *
+     * @param  DocumentSignature $document
+     * @return String
+     */
+    protected function generateFileWhichRegisteredStatusNull($document)
+    {
+        $path = config('sikd.base_path_file');
+        $file = $document->checkFile($path . 'ttd/draft/' . $document->tmp_draft_file);
+        if ($file === false) {
+            $file = $document->checkFile($path . 'ttd/sudah_ttd/' . $document->file);
+            if ($file === false) { // handle for old data before draft schema implemented
+                $file = $path . 'ttd/blm_ttd/' . $document->file;
             }
         }
-
-        $this->kafkaPublish('analytic_event', [
-            'event' => 'download_signed_letter',
-            'status' => KafkaStatusTypeEnum::SUCCESS(),
-            'letter' => [
-                'file' => $file
-            ]
-        ]);
-
-        return response()->json([
-            'file' => $file
-        ], 200);
+        return $file;
     }
 }
