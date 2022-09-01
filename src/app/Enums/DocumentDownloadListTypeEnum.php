e3bb0941a7070222a6584fea23cec72fa6b3014c
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-09-01 14:10:14 +0700

    Add dowonload document enums

---
+++ src/app/Enums/DocumentDownloadListTypeEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self INBOX()
+ * @method static self SIGNATURE()
+ */
+
+final class DocumentDownloadListTypeEnum extends Enum
+{
+}
