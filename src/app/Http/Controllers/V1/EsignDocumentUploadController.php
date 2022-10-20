abcbf68f56c58974d7f55ea6068c4085262b1d38
Indra Prasetya <indraprasetya154@gmail.com>
2022-10-20 14:47:36 +0700

    feat: store file to website service

--- src/app/Http/Controllers/V1/EsignDocumentUploadController.php
+++ src/app/Http/Controllers/V1/EsignDocumentUploadController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreEsignDocumentUploadRequest;
-use App\Models\DocumentSignature;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
@@ -18,26 +18,78 @@ class EsignDocumentUploadController extends Controller
      */
     public function upload(StoreEsignDocumentUploadRequest $request)
     {
-        $transfer = $this->doTransferFile($request);
+        $transferDocumentFile = $this->setDocumentFileUpload($request);
+        if ($transferDocumentFile->status() != Response::HTTP_OK) {
+            return response()->json([
+                'message' => 'Failed upload document file',
+                'status' => false,
+            ], 400);
+        }
+
+        $documentFile = json_decode($transferDocumentFile->getBody()->getContents());
+
+        $documentAttachment = null;
+        if ($request->hasFile('attachment')) {
+            $transferDocumentAttachmentFile = $this->setDocumentFileAttachmentUpload($request);
+            if ($transferDocumentAttachmentFile->status() != Response::HTTP_OK) {
+                return response()->json([
+                    'message' => 'Failed upload document attachment',
+                    'status' => false,
+                ], 400);
+            }
+
+            $documentAttachment = json_decode($transferDocumentAttachmentFile->getBody()->getContents());
+        }
+
+        $storeDocumentData = [
+            'data'       => $request->except(['file', 'attachment']),
+            'file'       => $documentFile->data->file,
+            'draft'      => $documentFile->data->draft,
+            'attachment' => ($documentAttachment != null) ? $documentAttachment->data->attachment : null,
+        ];
     }
 
-    protected function doTransferFile($request)
+    protected function setDocumentFileUpload($request)
     {
         $fileName = $request->file('file')->getClientOriginalName();
         $request->file('file')->storeAs('esign_document', $fileName);
+        $fileUpload = Storage::disk('local')->get('esign_document/' . $fileName);
 
-        $fileUpload = Storage::disk('esign_document')->get($fileName);
+        $bodyRequest = ['document_esign_type_id' => $request->document_esign_type_id];
+        $response = $this->doTransferFile('document_esign_file', $fileUpload, $fileName, $bodyRequest);
 
-        $documentRequest = [
-            'document_esign_type_id' => $request->document_esign_type_id
-        ];
+        Storage::disk('local')->delete('esign_document/' . $fileName);
 
-        $response = Http::withHeaders([
-            'Secret' => config('sikd.webhook_secret'),
-        ])->attach('document_esign_file', $fileUpload, $fileName)
-        ->attach('document_esign_attachment', $fileUpload, $fileName)
-        ->post(config('sikd.webhook_url'), $documentRequest);
+        return $response;
+    }
 
+    protected function setDocumentFileAttachmentUpload($request)
+    {
+        $fileNameAttachment = $request->file('attachment')->getClientOriginalName();
+        $request->file('file')->storeAs('esign_document/attachment', $fileNameAttachment);
+        $fileUploadAttachment = Storage::disk('esign_document/attachment')->get('esign_document/attachment/' . $fileNameAttachment);
 
+        $response = $this->doTransferFile('document_esign_attachment', $fileUploadAttachment, $fileNameAttachment);
+
+        Storage::disk('local')->delete('esign_document/attachment/' . $fileUploadAttachment);
+
+        return $response;
+    }
+
+    protected function doTransferFile($type, $fileUpload, $fileName, $bodyRequest = null)
+    {
+        try {
+            $response = Http::withHeaders([
+                'Secret' => config('sikd.webhook_secret'),
+            ])->attach($type, $fileUpload, $fileName)
+            ->post(config('sikd.webhook_url'), $bodyRequest);
+
+            return $response;
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Failed connect to storage on upload file ' . $type,
+                'status' => false,
+            ], 400);
+        }
     }
 }
